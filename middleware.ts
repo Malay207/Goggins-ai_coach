19698e99bd34bc51260850765bb36d792d9a0a00
Malay <108186647+Malay207@users.noreply.github.com>
2024-11-18 09:39:39 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -24,9 +24,7 @@ export default clerkMiddleware(async (auth, request) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)",
     // Add matcher to always allow public static files like manifest
     '/manifest.json',
     '/images/icons/(.*)',
