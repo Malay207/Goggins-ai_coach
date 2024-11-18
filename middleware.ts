4905b4697c3faab60732faca8780f6ef229c097a
malaykumar <malaydev207@gmail.com>
2024-11-18 09:48:03 +0530

    rootmiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
-  "/profile",
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/assistant/create',
@@ -24,9 +23,7 @@ export default clerkMiddleware(async (auth, request) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)",
     // Add matcher to always allow public static files like manifest
     '/manifest.json',
     '/images/icons/(.*)',
