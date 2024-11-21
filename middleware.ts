537fc9402a79cd11180aa0616bf069f1ccc745f0
malaykumar <malaydev207@gmail.com>
2024-11-22 00:17:38 +0530

    some changes

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/api/assistant/create',
-  '/api/thread',
-  '/api/message/create',
-  '/api/message/list',
-  '/api/run/create',
-  '/api/run/retrieve',
-  '/api/challenge-user',
-  '/api/openai',
-  '/manifest.json', // Ensure the manifest is treated as public
-  '/images/icons/(.*)', // Ensure icon assets are public
-]);
-export default clerkMiddleware(async (auth, request) => {
+const isPublicRoute = createRouteMatcher(['/', '/profile'])
+export  default  clerkMiddleware(async (auth, request) => {
+  
+  // if (!isPublicRoute(request) && request.nextUrl.pathname === '/') {
+  //   if ((await auth()).userId) {
+  //     return new Response(null, {
+  //       status: 307,
+  //       headers: { Location: '/profile' },
+  //     });
+  //   }
+  // }
   if (!isPublicRoute(request)) {
     await auth.protect();
   }
